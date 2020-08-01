c52ccb45481bb9a4b90a4cefd403567ab673b189
EakzIT <pilpaff@gmail.com>
2020-08-01 22:41:40 +0300

    Extracting logic from similar screens

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -5,39 +5,10 @@ import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
 const SignupScreen = ({ navigation }) => {
     const { state, signup } = useContext(AuthContext);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+
     return (
         <View style={styles.container}>
-            <Spacer>
-                <Text h3>Sign Up for Tracker</Text>
-            </Spacer>
-            <Spacer>
-                <Input
-                    label="Email"
-                    value={email}
-                    onChangeText={setEmail}
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                />
-            </Spacer>
-            <Input
-                secureTextEntry
-                label="Password"
-                value={password}
-                onChangeText={setPassword}
-                autoCapitalize="none"
-                autoCorrect={false}
-            />
-            {state.errorMessage ? (
-                <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-            ) : null}
-            <Spacer>
-                <Button
-                    title="Sign Up"
-                    onPress={() => signup({ email, password })}
-                />
-            </Spacer>
+
             <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
                 <Text style={styles.link}>
                     Already have an account? Sign in instead...
