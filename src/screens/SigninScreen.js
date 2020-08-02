2700d5cc4f05ab31a111116979abbb0437586da5
EakzIT <pilpaff@gmail.com>
2020-08-02 22:51:30 +0300

    Sign in screen visuals

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,33 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
 const SigninScreen = () => {
-    return <Text style={{ fontSize: 48  }}>SigninScreen</Text>;
+    return (
+        <View style={styles.container}>
+            <AuthForm
+                header="Sign In to Your Account"
+                errormessage=""
+                onSubmit={() => {}}
+                sumbitButtonText="Sign In"
+            />
+            <NavLink
+                text="Don't have an account? Sign up instead."
+                routeName="Signup"
+            />
+        </View>
+    );
 };
-
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = {
+    header: () => false,
+};
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        marginBottom: 250,
+    },
+});
 
 export default SigninScreen;
