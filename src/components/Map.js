6eb2cc29eb66f5b7bce4f2ca58576a0a15ba80af
EakzIT <pilpaff@gmail.com>
2020-08-07 12:48:46 +0300

    Map component progression. With polyline test

---
+++ src/components/Map.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import MapView, { Polyline } from "react-native-maps";
+
+const Map = () => {
+    let points = [];
+    const negPos=[1,-1];
+    for (let i = 0; i < 20; i++) {
+        points.push({
+            latitude: 52.052141 + (Math.random()*negPos[Math.floor(Math.random()*2)] * 0.001),
+            longitude: -1.223817 + (Math.random()*negPos[Math.floor(Math.random()*2)] * 0.001),
+        });
+    }
+    }
+    return (
+        <>
+            <MapView
+                initialRegion={{
+                    latitude: 52.052141,
+                    longitude: -1.223817,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                }}
+                style={styles.map}>
+                <Polyline coordinates={points}/>
+            </MapView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    map: {
+        height: 300,
+    },
+});
+
+export default Map;
